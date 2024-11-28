d728dd45ba611a109735bbcd9877570bb47c6103
yassineBenabda <benabdayassine395@gmail.com>
2024-11-28 22:08:18 +0100

    section21

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,22 +1,60 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { User } from '../model/user.model';
 import { Router } from '@angular/router';
+import { User } from '../model/user.model';
+import { JwtHelperService } from '@auth0/angular-jwt';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  users: User[] = [{ "username": "admin", "password": "123", "roles": ['ADMIN'] },
-                   { "username": "yassine", "password": "123", "roles": ['USER'] }];
+/*  users: User[] = [{"username":"admin","password":"123","roles":['ADMIN']},
+                   {"username":"nadhem","password":"123","roles":['USER']} ]; */
 
-  public loggedUser!: string;
-  public isloggedIn: Boolean = false;
-  public roles!: string[];
+ private helper = new JwtHelperService();
 
-  constructor(private router: Router) {}
-  
-  SignIn(user: User): Boolean {
+apiURL: string = 'http://localhost:8081/users';
+token!:string;
+
+public loggedUser!:string;
+public isloggedIn: Boolean = false;
+public roles!:string[];
+
+  constructor(private router : Router,
+              private http : HttpClient
+) { }
+
+  login(user : User)
+  {
+  return this.http.post<User>(this.apiURL+'/login', user , {observe:'response'});
+  }
+ 
+ saveToken(jwt:string){
+      localStorage.setItem('jwt',jwt);
+      this.token = jwt;
+      this.isloggedIn = true; 
+      this.decodeJWT();
+  }
+
+  getToken():string {
+    return this.token;
+  }
+
+  decodeJWT()
+  {   if (this.token == undefined)
+            return;
+    const decodedToken = this.helper.decodeToken(this.token);
+    this.roles = decodedToken.roles;
+    this.loggedUser = decodedToken.sub;
+  }
+
+ 
+
+
+ /* SignIn(user: User): Boolean {
     let validUser: Boolean = false;
     this.users.forEach((curUser) => {
       if (user.username == curUser.username && user.password == curUser.password) {
@@ -29,30 +67,49 @@ export class AuthService {
       }
     });
     return validUser;
-  }
-  isAdmin(): Boolean {
+  }*/
+
+  isAdmin():Boolean{
     if (!this.roles) //this.roles== undefiened
-      return false;
-    return (this.roles.indexOf('ADMIN') > -1);
-  }
+    return false;
+    return (this.roles.indexOf('ADMIN') >-1) ;
+    ;
+  }  
+
+
   logout() {
-    this.isloggedIn = false;
-    this.loggedUser = undefined!;
-    this.roles = undefined!;
-    localStorage.removeItem('loggedUser');
-    localStorage.setItem('isloggedIn', String(this.isloggedIn));
-    this.router.navigate(['/login']);
+  this.loggedUser = undefined!;
+  this.roles = undefined!;
+  this.token= undefined!;
+  this.isloggedIn = false;
+  localStorage.removeItem('jwt');
+  this.router.navigate(['/login']);
   }
+
   setLoggedUserFromLocalStorage(login: string) {
     this.loggedUser = login;
     this.isloggedIn = true;
-    this.getUserRoles(login);
+   // this.getUserRoles(login);
   }
-  getUserRoles(username: string) {
+
+  loadToken() {
+    this.token = localStorage.getItem('jwt')!;
+    this.decodeJWT();
+  }
+
+  isTokenExpired(): Boolean
+  {
+    return  this.helper.isTokenExpired(this.token);   
+  }
+
+
+
+  /*getUserRoles(username: string) {
     this.users.forEach((curUser) => {
       if (curUser.username == username) {
         this.roles = curUser.roles;
       }
     });
-  }
+  }*/
+    
 }
